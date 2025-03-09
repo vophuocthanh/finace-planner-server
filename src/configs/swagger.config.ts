12ce8d725396a028c3025b3e579444dd42ff9418
vophuocthanh <phuocthanh2k03@gmail.com>
2025-03-10 01:15:07 +0700

    Config swagger 🔥

--- src/configs/swagger.config.ts
+++ src/configs/swagger.config.ts
@@ -3,26 +3,29 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const APP_TITLE = 'Finance Planner';
 const SWAGGER_PATH = 'api';
+const API_TAGS = [
+  'Auth',
+  'Budgets',
+  'Categories',
+  'Expenses',
+  'User',
+  'Monthlies',
+  'Investments',
+  'Personal Incomes',
+  'Savings',
+  'Role',
+];
 
-export const setupSwagger = (app: NestExpressApplication) => {
+export const setupSwagger = (app: NestExpressApplication): void => {
   const config = new DocumentBuilder()
     .setTitle(APP_TITLE)
     .setDescription(`${APP_TITLE} API description`)
     .setVersion('1.0')
-    .addTag('Auth')
-    .addTag('Budgets')
-    .addTag('Categories')
-    .addTag('Expenses')
-    .addTag('User')
-    .addTag('Monthlies')
-    .addTag('Investments')
-    .addTag('Personal Incomes')
-    .addTag('Savings')
-    .addTag('Role')
-    .addBearerAuth()
-    .build();
+    .addBearerAuth();
 
-  const document = SwaggerModule.createDocument(app, config);
+  API_TAGS.forEach((tag) => config.addTag(tag));
+
+  const document = SwaggerModule.createDocument(app, config.build());
   SwaggerModule.setup(SWAGGER_PATH, app, document, {
     customSiteTitle: `Swagger | ${APP_TITLE}`,
   });
